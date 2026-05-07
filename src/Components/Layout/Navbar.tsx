e5acfaa0ce1e9dbfb9a7ba31cde4568b7645816a
othmaneslaimani2-jpg <othmaneslaimani2@gmail.com>
2026-05-07 16:38:35 +0100

    finished hero section

--- src/Components/Layout/Navbar.tsx
+++ src/Components/Layout/Navbar.tsx
@@ -1,14 +1,14 @@
 export const Header = () => {
 return (
-<header className="flex items-center justify-between">
+<header className="flex items-center justify-between ">
 
-<img className="flex size-10/100" src="\src\assets\Logo.png" alt="Logo Mawazine Beat"/>
-<div className=" z-40">
-    <ul className="flex gap-4 text-white hover:text-#80F915">
-    <li>Home</li>
-<li>Program</li>
-    <li>Schedule</li>
-    <li>Passport</li>
+<img className="flex cursor-pointer size-10/100" src="\src\assets\Logo.png" alt="Logo Mawazine Beat"/>
+<div className=" z-40 cursor-pointer">
+    <ul className="flex gap-6 text-white font-medium">
+    <li className="hover:text-[#ff1111]">Home</li>
+<li className="hover:text-[#ff1111]">Program</li>
+    <li className="hover:text-[#ff1111]">Schedule</li>
+    <li className="hover:text-[#ff1111]">Passport</li>
     </ul>
     </div>
 </header>
